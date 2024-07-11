fb8cdc5d045ce95127538abec64869ef53b49669
Moropane Kuullo Innocent <kimoropana823@gmail.com>
2024-07-11 14:46:37 +0200

    search addede

--- UI/src/pages/employee/employee.jsx
+++ UI/src/pages/employee/employee.jsx
@@ -29,19 +29,25 @@ export default function Employee(){
                 em.map(e =>
                 <div className='emp'  key={e.id} >
                     <h1>{e.position}</h1>
-                    <h3>Name: {e.name}</h3> 
-                    <h4>Surname: {e.surname}</h4>
-                    <h4>Age: {e.Age}</h4>
-                    <h4>Email: {e.email}</h4>
-                    <h4>Phone: {e.phone}</h4>
+                    {e.name ? <h3>Name: {e.name}</h3>:null}
+                    { e.surname ? <h4>Surname: {e.surname}</h4>:null}
+                    { e.Age ? <h4>Age: {e.Age}</h4>:null}
+                    { e.email ? <h4>Email: {e.email}</h4>:null}
+                    { e.phone ? <h4>Phone: {e.phone}</h4>:null}
 
 
                     <div className='forms'>
+                    { e.name?
                       <form action="edit.html" onSubmit={setSubmit} id={e.id}>
-                        <button type='edit' onClick={()=> editSet(e.id)}>Edit</button>
+                      <button type='edit' onClick={()=> editSet(e.id)} >Edit</button> 
                      </form>
+                     : null }
+                     <form action="employees.html">
+                        <button>Back</button>
+                     </form>
+                    
                         <form action="">  
-                        <button onClick={() => deleteHandle(e.id)} type='delete'>Delete</button>
+                        <button onClick={() => deleteHandle(e.id)} type='delete' >Delete</button>
                         </form> 
                     </div>
 
