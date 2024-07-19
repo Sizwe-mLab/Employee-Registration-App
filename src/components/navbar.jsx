d068dcef188ea681d5ccdc2697aa4b3f402d0c18
Kutullo innocent Moropane <49004939+IKcracker@users.noreply.github.com>
2024-07-19 09:18:35 +0200

    updating logo

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,7 +2,7 @@ export default function Navbar(){
   return (
       <nav className="navbar navbar-expand-lg bg-body-tertiary">
            <div className="container-fluid">
-          <a className="navbar-brand" href="index.html">Admin</a>
+          <a className="navbar-brand" href="/employees">Admin</a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
           </button>
